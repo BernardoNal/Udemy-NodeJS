04b6411e3f8b624461cac2ef289fdb0675f033e1
BernardoNal <nal-carvalho@hotmail.com>
2022-01-14 23:57:25 -0300

    Seção 10 Concluída

--- portal _de_noticias/app/routes/admin.js
+++ portal _de_noticias/app/routes/admin.js
@@ -1,9 +1,23 @@
 module.exports=function(App){
     App.get('/formulario_noticia',function(req,res){
-        res.render('admin/form_add_noticia');
+        res.render('admin/form_add_noticia',{validacao :{},noticia:{}});
     })
     App.post('/noticias/salvar',function(req,res){
         var noticia = req.body;
+
+        req.assert('titulo','Título é obrigatório').notEmpty();
+        req.assert('resumo','Resumo é obrigatório').notEmpty();
+        req.assert('resumo','Resumo deve conter entre 1 e 100').len(1,100);
+        req.assert('autor','Autor é obrigatório').notEmpty();
+        req.assert('data_noticia','A data é obrigatória').notEmpty();
+        req.assert('noticia','A notícia é obrigatória').notEmpty();
+
+        var erros = req.validationErrors();
+
+        if(erros){
+            res.render('admin/form_add_noticia',{validacao : erros , noticia : noticia});
+            return;
+        }
        
         var connection = App.config.dbConnection();
         var noticiasModel = new App.app.models.noticiasModels(connection);
